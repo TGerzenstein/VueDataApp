9b80166a201e67e5e160342c3f7518e842f1dd3b
Tatiana <tatigerzens@gmail.com>
2024-09-25 10:22:33 -0300

    Refactored App and List components to display product listings

--- VueDataApp/src/App.vue
+++ VueDataApp/src/App.vue
@@ -1,10 +1,68 @@
 <script setup lang="ts">
-import PageHeader from './components/PageHeader.vue'
+
+import PageHeader from './components/PageHeader.vue';
+import ListProducts from './components/ListProducts.vue'
+import ProductDescription from './components/ProductDescription.vue'
+
+import { onMounted, ref } from 'vue';
+import axiosClient from './utils/axios';
+
+import type { Product } from './models/products.model'
+
+
+const products = ref<Product[]>([]);
+
+//Get products
+const fetchProducts = async() => {
+    try {
+      const { data } = await axiosClient.get("/search?q=tecnologia");
+      products.value = data.results; 
+      console.log('componente ejecutado')  
+
+    } catch(error) {
+      console.log(error);
+    }
+  }
+  
+  onMounted(() => {
+      fetchProducts();
+  });
+
+
+//Filter products
+let input = ref("");
+
+function filteredList() {
+    console.log("searching for: ", input.value);
+}
+
 
 </script>
 
 <template>
+      <header class="flex h-12 bg-yellow-300 px-4">
+        <form action="" class="flex m-auto max-w-screen-xl flex-1" @submit.prevent="filteredList">
+            <input name="search" 
+                    class="bg-white h-8 flex-1 mr-2 px-2" 
+                    type="text"
+                    placeholder="Search products"
+                    v-model="input"
+                    @input="filteredList"
+                    >
+            <button class="bg-gray-300 px-2 py-1 text-slate-700">
+                Search
+            </button>
+        </form>
+    </header>
 
-  <PageHeader />
+
+    <!--   
+      <PageHeader /> -->
+    <div class="container mx-auto px-4 max-w-screen-xl">
+        <ListProducts :products="products" />
+
+    </div>  
+<!--       <ProductDescription />
+ -->
 </template>
 
--- VueDataApp/src/components/ListProducts.vue
+++ VueDataApp/src/components/ListProducts.vue
@@ -1,52 +1,31 @@
 <script setup lang="ts">
 
-import { onMounted, ref } from 'vue';
-import axiosClient from '../utils/axios';
+import { defineProps } from 'vue';
+import type { Product } from '../models/products.model'
 
 
-//My interface
-interface Product {
-  id: number;
-  title: string;
-  condition: string;
-  thumbnail: string;
-  price: string;
+interface Props {
+    products: Product[];
 }
 
-const products = ref<Product[]>([]);
-
-
-const fetchProducts = async() => {
-    try {
-      const { data } = await axiosClient.get("/search?q=tecnologia");
-      products.value = data.results; 
-      console.log('componente ejecutado')  
-
-    } catch(error) {
-      console.log(error);
-    }
-  }
-  
-  onMounted(() => {
-      fetchProducts();
-  });
+defineProps<Props>();
 </script>
 
 
 <template>
-      <div>
-        <ul class="flex flex-wrap justify-center">
-          <li  class="w-64 max-w-sm rounded overflow-hidden shadow-lg m-4" 
-              v-for="product in products" :key="product.id">
+      <section>
+          <ul class="flex flex-wrap">
+            <li  class="w-64 max-w-sm rounded border overflow-hidden shadow-lg m-2" 
+                v-for="product in products" :key="product.id">
 
-            <div class="px-6 py-4">
-              <p class="m-1">{{ product.condition }}</p>
-              <h3 class="font-bold text-xl mb-2">{{ product.title }}</h3>
-              <p class="font-bold">{{ product.price }}</p>
-            </div>
-            <img :src="product.thumbnail" class="m-auto" alt="">      
-          </li>
-        </ul>
-      </div>
-
+              <div class="px-6 py-4">
+                <p class="m-1">{{ product.condition }}</p>
+                <h3 class="font-bold text-xl mb-2">{{ product.title }}</h3>
+                <p class="font-bold">{{ product.price }}</p>
+                <p>{{ product.currency_id }}</p>
+              </div>
+              <img :src="product.thumbnail" class="m-auto" alt="">      
+            </li>
+          </ul>
+      </section>
 </template>
