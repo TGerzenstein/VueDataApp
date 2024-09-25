552362bac2c6d140c3403bbfdfb6f0103511af8e
Tatiana <tatigerzens@gmail.com>
2024-09-25 13:54:41 -0300

    updated components

--- VueDataApp/src/components/ListProducts.vue
+++ VueDataApp/src/components/ListProducts.vue
@@ -2,7 +2,7 @@
 
 import { defineProps } from 'vue';
 import type { Product } from '../models/products.model'
-
+import  ProductDescription from './ProductDescription.vue'
 
 interface Props {
     products: Product[];
@@ -14,18 +14,13 @@ defineProps<Props>();
 
 <template>
       <section>
-          <ul class="flex flex-wrap">
-            <li  class="w-64 max-w-sm rounded border overflow-hidden shadow-lg m-2" 
-                v-for="product in products" :key="product.id">
 
-              <div class="px-6 py-4">
-                <p class="m-1">{{ product.condition }}</p>
-                <h3 class="font-bold text-xl mb-2">{{ product.title }}</h3>
-                <p class="font-bold">{{ product.price }}</p>
-                <p>{{ product.currency_id }}</p>
-              </div>
-              <img :src="product.thumbnail" class="m-auto" alt="">      
-            </li>
-          </ul>
+        <ul class="flex flex-wrap">
+           <li  class="w-64 max-w-sm rounded border overflow-hidden shadow-lg m-2"
+                v-for="product in products"
+           >
+           <ProductDescription :product="product" />
+           </li>
+        </ul>
       </section>
 </template>
---
+++ VueDataApp/src/components/ProductDescription.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import { defineProps } from 'vue';
+import type { Product } from '../models/products.model'
+
+
+interface Props {
+    product: Product;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+        <div class="px-6 py-4">
+            <p class="m-1">{{ product.condition }}</p>
+                <h3 class="font-bold text-xl mb-2">{{ product.title }}</h3>
+                <p class="font-bold">{{ product.price }}</p>
+                <p>{{ product.currency_id }}</p>
+        </div>
+</template>
