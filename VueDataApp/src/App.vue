75f9be902c409dba0a5a453387dd3670d9f1a870
Tatiana <tatigerzens@gmail.com>
2024-09-25 13:53:30 -0300

    Implemented a search filter to dynamically filter products by title

--- VueDataApp/src/App.vue
+++ VueDataApp/src/App.vue
@@ -2,7 +2,6 @@
 
 import PageHeader from './components/PageHeader.vue';
 import ListProducts from './components/ListProducts.vue'
-import ProductDescription from './components/ProductDescription.vue'
 
 import { onMounted, ref } from 'vue';
 import axiosClient from './utils/axios';
@@ -11,58 +10,61 @@ import type { Product } from './models/products.model'
 
 
 const products = ref<Product[]>([]);
+const search = ref("");
+const filteredProducts = ref<Product[]>([]);
+
+
 
 //Get products
 const fetchProducts = async() => {
     try {
       const { data } = await axiosClient.get("/search?q=tecnologia");
       products.value = data.results; 
+      filteredProducts.value = products.value; 
       console.log('componente ejecutado')  
 
     } catch(error) {
       console.log(error);
     }
   }
-  
-  onMounted(() => {
+
+
+const filterProducts = () => {
+    filteredProducts.value = products.value.filter((product) => 
+    product.title.toLowerCase().includes(search.value.trim().toLocaleLowerCase())
+    );
+};
+
+onMounted(() => {
       fetchProducts();
   });
 
 
-//Filter products
-let input = ref("");
-
-function filteredList() {
-    console.log("searching for: ", input.value);
-}
-
-
 </script>
+
+
+
 
 <template>
       <header class="flex h-12 bg-yellow-300 px-4">
-        <form action="" class="flex m-auto max-w-screen-xl flex-1" @submit.prevent="filteredList">
+        <form action="" class="flex m-auto max-w-screen-xl flex-1">
             <input name="search" 
                     class="bg-white h-8 flex-1 mr-2 px-2" 
                     type="text"
                     placeholder="Search products"
-                    v-model="input"
-                    @input="filteredList"
+                    v-model="search"
+                    @input="filterProducts"
                     >
-            <button class="bg-gray-300 px-2 py-1 text-slate-700">
+                    <button class="bg-gray-300 px-2 py-1 text-slate-700">
                 Search
             </button>
         </form>
     </header>
+<!--     <PageHeader /> -->
 
-
-    <!--   
-      <PageHeader /> -->
     <div class="container mx-auto px-4 max-w-screen-xl">
-        <ListProducts :products="products" />
+        <ListProducts :products="filteredProducts.length > 0 ? filteredProducts : products" />
 
     </div>  
-<!--       <ProductDescription />
- -->
 </template>
 
