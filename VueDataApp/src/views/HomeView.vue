c617b778b329525bd335db8b0e8b07d738a019cf
Tatiana <tatigerzens@gmail.com>
2024-10-14 12:33:58 -0300

    updated home view

--- VueDataApp/src/views/HomeView.vue
+++ VueDataApp/src/views/HomeView.vue
@@ -19,7 +19,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <header class="flex h-12 bg-orange-500 px-4">
+    <header class="flex h-12 bg-blue-700 px-4">
       <form action="" class="flex m-auto max-w-screen-xl flex-1">
         <input name="search" class="bg-white h-8 flex-1 mr-2 px-2" type="text" placeholder="Search"
           >
